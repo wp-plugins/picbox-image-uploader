de71ba09c8994669bc6fcc2b18137fd686eb8164
NegaCommando <NegaCommando@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-09 23:01:08 +0000

    fixed error
    git-svn-id: https://plugins.svn.wordpress.org/picbox-image-uploader/trunk@665735 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- picBox.php
+++ picBox.php
@@ -5,14 +5,14 @@
  Plugin Name: picBox Image Uploader
  Plugin URI: http://picbox.us
  Description: Upload images to picBox in  a post
- Version: 0.2
+ Version: 0.1
  Author: NegaCommando
  Author URI: http://negacraft.twilightparadox.com
 */
 function picBox_uploader(){
 echo '		
             <p id="upload"><h3 class="media-title">Upload images to picBox.us</h3></p>
-			<iframe onload="getdata" src="https://picbox.us/iframe_mini.php?mini=2" width="510" height="75" scrolling="no" frameborder="0" allowtransparency="true" id="miniupload"></iframe>
+			<iframe src="https://picbox.us/iframe_mini.php?mini=2" width="510" height="75" scrolling="no" frameborder="0" allowtransparency="true" id="miniupload"></iframe>
             
             
             
@@ -20,11 +20,6 @@ echo '
 }
 add_action('post-upload-ui', 'picBox_uploader');
 
-function getdata(){
-    <script>
-    var url = document.getElementById("miniupload").contentDocument.body
-    echo $url
-    }
     
 ?>
 
