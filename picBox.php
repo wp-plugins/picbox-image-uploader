323f1fe924d6645c7cf346c0fcb5c64d125e598f
NegaCommando <NegaCommando@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-15 02:59:09 +0000

    Removed more nonsense from code.
    git-svn-id: https://plugins.svn.wordpress.org/picbox-image-uploader/trunk@668199 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- picBox.php
+++ picBox.php
@@ -12,8 +12,8 @@
 function picBox_uploader(){
 echo '		
             <div align="center">
-            <p id="upload"><h3 class="media-title">Upload images to picBox.us</h3></p>
-			<iframe src="https://picbox.us/iframe_mini.php?mini=2" width="510" height="75" scrolling="no" frameborder="0" allowtransparency="true" </iframe></div>';
+            <p><h3>Upload images to picBox.us</h3></p>
+			<iframe src="https://picbox.us/iframe_mini.php?mini=2" width="510" height="75" scrolling="no" frameborder="0" allowtransparency="true"</iframe></div>';
             
 }
 add_action('post-upload-ui', 'picBox_uploader');
