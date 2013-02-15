08c04e0bbaad11a2324022b0be61583a77c9be2d
NegaCommando <NegaCommando@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-15 02:43:52 +0000

    Removed an extra "<" that was causing an issue, giving an unexpected output error message.
    git-svn-id: https://plugins.svn.wordpress.org/picbox-image-uploader/trunk@668189 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- picBox.php
+++ picBox.php
@@ -5,14 +5,15 @@
  Plugin Name: picBox Image Uploader
  Plugin URI: http://picbox.us
  Description: Upload images to picBox in  a post
- Version: 0.1
+ Version: 0.1b
  Author: NegaCommando
  Author URI: http://negacraft.twilightparadox.com
 */
 function picBox_uploader(){
 echo '		
             <p id="upload"><h3 class="media-title">Upload images to picBox.us</h3></p>
-			<iframe src="https://picbox.us/iframe_mini.php?mini=2" width="510" height="75" scrolling="no" frameborder="0" allowtransparency="true" id="miniupload"></iframe>
+			<iframe src="https://picbox.us/iframe_mini.php?mini=2" width="510" height="75" scrolling="no" frameborder="0" allowtransparency="true" id="miniupload"</iframe>
+           
             
             
             
