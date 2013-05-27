6db6d0f2e53a048560f7bd7681bc78ed1f71da62
NegaCommando <NegaCommando@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-27 20:33:51 +0000

    Changed upload from an iframe to a Javascript pop-up uploader window. Removed the src folder, and put the CSS and JS on MunyCDN. Began work on a function to insert the uploaded image into the post. frame.css has been removed as it is no longer needed, obviously because the iframe was removed. The button for opening the upload popup is using Twitter Bootstrap's bootstrap-button.js
    git-svn-id: https://plugins.svn.wordpress.org/picbox-image-uploader/trunk@719048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- picBox.php
+++ picBox.php
@@ -5,23 +5,44 @@
  Plugin Name: picBox Image Uploader
  Plugin URI: http://picbox.us
  Description: Upload images to picBox in  a post
- Version: 1.0.0
+ Version: 1.1
  Author: NegaCommando
  Author URI: http://home.oregongarlic.net
 */
 function picBox_uploader(){
 echo '
-  <link rel="stylesheet" href="frame.css"/>
+ <link href="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-responsive.css" rel="stylesheet">
             <div align="center">
             <p><h3>Upload images to picBox.us</h3></p>
-			<iframe id="upload" src="https://picbox.us/ext_upload" scrolling="no" frameborder="0" allowtransparency="true"</iframe></div>';
+            	<a href="https://picbox.us/ext_upload" onclick="javascript:void window.open(\'https://picbox.us/ext_upload\',\'1369681531638\',\'width=434,height=226,toolbar=0,menubar=0,location=0,status=0,scrollbars=0,resizable=0,left=0,top=0\');return false;" class="btn btn-primary btn-large"><i class="icon-white icon-upload"></i><font color="white"> Upload to picBox</font></a>
+   <!--   <form action="pB_insert()" method="post">
+    <input type="text" name="imageurl" placeholder="Image URL">
+    </form>
+    -->
+    
+    
+    <!--BootStrap JS-->             
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/jquery.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-transition.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-alert.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-modal.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-dropdown.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-scrollspy.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-tab.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-tooltip.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-popover.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-button.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-collapse.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-carousel.js"></script>
+    <script src="https://munycdn.com/a/picBoxWordPressPlugin/src/bootstrap-typeahead.js"></script>';
             
             
-            
-            
-            
-            
-
 }
 add_action('post-upload-ui', 'picBox_uploader');
 
